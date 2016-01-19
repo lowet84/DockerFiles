8dc045f75116fe16123bf030bb53fcbc2049bf32
fredrik <lowet84@gmail.com>
2016-01-19 20:24:57 +0100

    fdgluijdfkgu

--- plex/build.sh
+++ plex/build.sh
@@ -1,2 +1,2 @@
-udo docker rmi -f lowet84/plex
+sudo docker rmi -f lowet84/plex
 sudo docker build -t lowet84/plex .
--- plex/start.sh
+++ plex/start.sh
@@ -1,1 +1,2 @@
-sudo docker run -it -d -p 192.168.1.131:32400:32400 lowet84/plex
+docker rm -f plex
+docker run --restart=always --privileged -d --name plex --net="host" -v /home/fredrik/plex/:/config -v /home/fredrik/filer/:/data -p 192.168.1.131:32400:32400  lowet84/plex
