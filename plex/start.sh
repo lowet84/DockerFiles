3af414d388d57532d226e5f976886104a5298d47
fredrik <lowet84@gmail.com>
2016-01-20 09:49:28 +0100

    difj

--- plex/start.sh
+++ plex/start.sh
@@ -1,2 +1,2 @@
 docker rm -f plex
-docker run --restart=always --privileged -d --name plex --net="host" -v /home/fredrik/plex/:/config -v /home/fredrik/filer/:/data -p 192.168.1.131:32400:32400  lowet84/plex
+docker run --restart=always --privileged --name plex --net="host" -v /home/fredrik/plex/:/config -v /home/fredrik/filer/:/data -p 192.168.1.131:32400:32400  lowet84/plex
