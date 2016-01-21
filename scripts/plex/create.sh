5f073589e9555785dd51e9532df8019c9b1894b6
lowet84 <lowet84@gmail.com>
2016-01-21 16:42:17 +0100

    dsf
    sdf

--- scripts/plex/create.sh
+++ scripts/plex/create.sh
@@ -2,4 +2,4 @@
 DATA=$1
 
 sudo docker run -it --name plex_data -v $DATA:/data/ lowet84/plex echo "plex data"
-sudo docker run -it --name plex -d --volumes-from plex_data --privileged --net="host" -p 32400:32400 lowet84/plex 
+sudo docker run -it --name plex --restart always -d --volumes-from plex_data --privileged --net="host" -p 32400:32400 lowet84/plex 
