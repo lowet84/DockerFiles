342cfd15a3ae2a4456ca0ca6524b641f66d162c5
fredrik <lowet84@gmail.com>
2016-01-21 14:12:59 +0100

    sdfr

--- scripts/plex/plex.sh
+++ scripts/plex/plex.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DATA=$1
 
-sudo docker run -it --name plex_data -v /home/fredrik/filer/:/data/ lowet84/plex echo "plex data"
+sudo docker run -it --name plex_data -v $DATA:/data/ lowet84/plex echo "plex data"
 sudo docker run -it --name plex -d --volumes-from plex_data --privileged --net="host" -p 32400:32400 lowet84/plex 
