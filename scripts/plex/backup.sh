be975f2c64453cf6d5614765758f711b4dc6f15b
fredrik <lowet84@gmail.com>
2016-01-21 14:22:02 +0100

    pdsojlisdf

--- scripts/plex/backup.sh
+++ scripts/plex/backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-BACKUPPATH=$1
-
 sudo docker stop plex
 sudo docker run -i --rm --volumes-from plex_data -w /config lowet84/plex 7z a backup.7z
+sudo docker cp plex_data:/config/backup.7z .
  
 sudo docker start plex
