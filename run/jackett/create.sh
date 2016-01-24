bd446574a5ba61b556e588d35e1eb2dff1d52c98
lowet84 <lowet84@gmail.com>
2016-01-24 23:46:57 +0100

    Update create.sh

--- run/jackett/create.sh
+++ run/jackett/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #sudo docker run -v /root/.config/Jackett-public --name jackett_data lowet84/storage echo "jackett data"
-sudo docker run --net="host" -it -d --name jackett -p 9117:9117 lowet84/jackett
+sudo docker run --net="host" -it -d --name jackett -p 9117:9117 guillaumegl/docker-jackett
