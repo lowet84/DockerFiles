b552b16c7fb99c8ffc947ceff20a43e5d84da2fe
lowet84 <lowet84@gmail.com>
2016-01-27 10:13:36 +0100

    dfulhsdf

--- run/destroyAll.sh
+++ run/destroyAll.sh
@@ -2,3 +2,4 @@ sudo sh plex/destroy.sh
 sudo sh sonarr/destroy.sh
 sudo sh jackett/destroy.sh
 sudo sh deluge/destroy.sh
+sudo sh couchpotato/destroy.sh
--- run/restoreAll.sh
+++ run/restoreAll.sh
@@ -3,4 +3,4 @@ SOURCE=$1
 sudo sh plex/restore.sh $SOURCE/plex_backup.7z
 sudo sh sonarr/restore.sh $SOURCE/sonarr_backup.7z
 sudo sh deluge/restore.sh $SOURCE/deluge_backup.7z
-
+sudo sh couchpotato/restore.sh $SOURCE/couchpotato_backup.7z
