d907b51ba7719d0eb06726dd31276f19f25d17eb
root <lowet84@gmail.com>
2016-02-04 17:48:54 +0100

    sdfsfsd

---
+++ build2/buildall.sh
@@ -0,0 +1,27 @@
+cd sambabase
+sh build.sh
+cd ..
+
+cd mono
+sh build.sh
+cd ..
+
+cd couchpotato  
+sh build.sh
+cd ..
+
+cd filehub
+sh build.sh
+cd ..
+
+cd jackett
+sh build.sh
+cd ..
+
+cd plex
+sh build.sh
+cd ..
+
+cd sonarr
+sh build.sh
+cd ..
---
+++ build2/pushall.sh
@@ -0,0 +1,7 @@
+docker push couchpotato  
+docker push filehub 
+docker push jackett 
+docker push mono 
+docker push plex 
+docker push sambabase  
+docker push sonarr
