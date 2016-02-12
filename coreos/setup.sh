927b75210b3f252d1d19896065c05084b443d221
root <lowet84@gmail.com>
2016-02-12 17:05:30 +0100

    dfgdfg

--- coreos/setup.sh
+++ coreos/setup.sh
@@ -1,5 +1,6 @@
-wget https://raw.githubusercontent.com/lowet84/DockerFiles/master/coreos/cloud-config-file
+wget -q https://raw.githubusercontent.com/lowet84/DockerFiles/master/coreos/cloud-config-file.template -O cloud-config-file
 
 PASSKEY=$(openssl passwd -1)
 
-ed -i -- 's/replace-this/'$PASSKEY'/g' cloud-config-file
+sed -i -- 's/replace-this/'$PASSKEY'/g' cloud-config-file
+sudo coreos-install -d /dev/sda -C stable -c cloud-config-file
