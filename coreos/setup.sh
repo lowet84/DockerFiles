ec09985926f047ecbb5245b5a3cab58ecc5280d8
root <lowet84@gmail.com>
2016-02-12 16:49:11 +0100

    sdfsdfsd

--- coreos/setup.sh
+++ coreos/setup.sh
@@ -1,1 +1,5 @@
+wget https://raw.githubusercontent.com/lowet84/DockerFiles/master/coreos/cloud-config-file
 
+PASSKEY=$(openssl passwd -1)
+
+ed -i -- 's/replace-this/'$PASSKEY'/g' cloud-config-file
