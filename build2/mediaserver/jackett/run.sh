15c5a6442d732e799c6253bebbdce2ce921e6bcb
lowet84 <lowet84@gmail.com>
2016-03-15 14:45:28 +0100

    Update run.sh

--- build2/mediaserver/jackett/run.sh
+++ build2/mediaserver/jackett/run.sh
@@ -1,2 +1,2 @@
 docker rm -f jackett
-sudo docker run --restart=always --net=lowet84-mediaserver -it -d --name jackett -p 9117:9117 -v /volumes/jackett:/root/.config/Jackett -e JACKETT_KEY=d7nc8w37y4mc9348u9q83mqv lowet84/jackett
+sudo docker run --restart=always --net=lowet84-mediaserver -it -d --name jackett -p 9117:9117 -v /volumes/mediaserver/jackett:/root/.config/Jackett -e JACKETT_KEY=d7nc8w37y4mc9348u9q83mqv lowet84/jackett
