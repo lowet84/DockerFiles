f020b646fc3096358266a7dd14b31f893354878b
lowet84 <lowet84@gmail.com>
2016-03-15 14:45:07 +0100

    Update run.sh

--- build2/mediaserver/deluge/run.sh
+++ build2/mediaserver/deluge/run.sh
@@ -1,2 +1,2 @@
 docker rm -f deluge
-docker run --privileged --restart=always --name deluge -d --net=lowet84-mediaserver -it -v /volumes/deluge:/config -v /volumes/mount.sh:/update/mount.sh -p 8112:8112 lowet84/deluge
+docker run --privileged --restart=always --name deluge -d --net=lowet84-mediaserver -it -v /volumes/mediaserver/deluge:/config -v /volumes/mediaserver/mount.sh:/update/mount.sh -p 8112:8112 lowet84/deluge
