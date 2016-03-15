af2ad31927e20f46b7fa92570c1a9c40ba073bff
root <lowet84@gmail.com>
2016-03-15 12:30:44 +0100

    compose

---
+++ build2/subsonic/run.sh
@@ -0,0 +1,2 @@
+docker rm -f subsonic
+docker run --privileged --restart=always --name subsonic -d --net=lowet84-mediaserver -it -v /volumes/subsonic:/config -v /volumes/mount.sh:/update/mount.sh -p 4040:4040 lowet84/subsonic
