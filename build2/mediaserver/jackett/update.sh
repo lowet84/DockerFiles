8a3db46f5678b62aa91eb416ab5197e0230db349
lowet84 <lowet84@gmail.com>
2016-03-29 09:55:44 +0200

    Update update.sh

--- build2/mediaserver/jackett/update.sh
+++ build2/mediaserver/jackett/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION=$(curl -s https://api.github.com/repos/lowet84/Jackett/releases/latest | grep 'tag_name' | cut -d\" -f4)
-DOWNLOADURL=$(curl -s https://api.github.com/repos/lowet84/Jackett/releases/latest | grep 'browser_download_url.*Jackett\.zip' | cut -d\" -f4)
-ls
+VERSION=$(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest | grep 'tag_name' | cut -d\" -f4)
+DOWNLOADURL=$(curl -s https://api.github.com/repos/Jackett/Jackett/releases/latest | grep 'browser_.*Binaries.Windows.zip' | cut -d\" -f4)
+
 if [ -f version_$VERSION ]
 then
   echo "version is already latest"
