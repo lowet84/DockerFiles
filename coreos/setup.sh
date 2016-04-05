750bf83b116326b9a4e219706a85ab6c14d883eb
Fredrik Löwenhamn <lowet84@gmail.com>
2016-04-05 14:01:23 +0200

    Update setup.sh

--- coreos/setup.sh
+++ coreos/setup.sh
@@ -1,2 +1,2 @@
 wget goo.gl/mdaH3F -O cloud-config-file
-sudo coreos-install -d /dev/sda -C alpha -c cloud-config-file -V $VERSION
+sudo coreos-install -d /dev/sda -C alpha -c cloud-config-file -V $1
