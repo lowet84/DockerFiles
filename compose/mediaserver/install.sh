b8772e7b66aa4b4557f67f5e7bd22066fc406925
Fredrik Löwenhamn <lowet84@gmail.com>
2016-08-24 14:32:38 +0200

    Update install.sh

--- compose/mediaserver/install.sh
+++ compose/mediaserver/install.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+echo "Enter DOMAIN"
+read domain
+echo "Enter SERVER"
+read server
+echo "Enter SMBUSER"
+read user
+echo "Enter SMBPASS"
+read -s pass
+echo "Enter SMBFOLDER"
+read folder
+echo "Enter MNTPATH"
+read path
+
+wget -q https://raw.githubusercontent.com/lowet84/DockerFiles/master/compose/mediaserver/docker-compose.yml -O docker-compose.yml
+DOMAIN=$domain SERVER=$server SMBUSER=$user SMBPASS=$pass SMBFOLDER=$folder MNTPATH=$path docker-compose up -d
